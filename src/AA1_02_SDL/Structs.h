2241c8bbf91dfa01c9497cc59f2090a3e04d5825
Martí <marti.deferrer@enti.cat>
2019-10-07 23:48:01 +0200

    ClassVersion

--- src/AA1_02_SDL/Structs.h
+++ src/AA1_02_SDL/Structs.h
@@ -13,23 +13,36 @@ struct Vec2 {
 
 
 //Buttons main menu manager
-void ButtonsManager(SDL_Rect &soundRect, SDL_Rect &playRect, SDL_Rect &exitRect, SDL_Texture *sText, SDL_Texture *pText, SDL_Texture *eText, SDL_MouseButtonEvent mouse,bool &isRunning) {
+void ButtonsMenuManager(SDL_Rect &soundRect, SDL_Rect &playRect, SDL_Rect &exitRect, SDL_Texture *sText, SDL_Texture *pText, SDL_Texture *eText, bool &cLeft,bool &isRunning, Mix_Music *sTrack) {
 	
 	//Exit
-	if (mousePos.x < (exitRect.x + exitRect.w) && mousePos.x>exitRect.x && mousePos.y < (exitRect.y + exitRect.y) && mousePos.y > exitRect.y)
+	if (mousePos.x < (exitRect.x + exitRect.w) && mousePos.x>exitRect.x && mousePos.y  < (exitRect.y + exitRect.h) && mousePos.y > exitRect.y) {	
 		SDL_SetTextureColorMod(eText, 255, 0, 0);
-	else SDL_SetTextureColorMod(eText, 150, 0, 0);
+		if (cLeft) isRunning = false;
+	}	else SDL_SetTextureColorMod(eText, 150, 0, 0);
 
 	//Play
-	if (mousePos.x < (playRect.x + playRect.w) && mousePos.x>playRect.x && mousePos.y < (playRect.y + playRect.y) && mousePos.y > playRect.y)
-		SDL_SetTextureColorMod(pText, 100, 100, 100);
+	if (mousePos.x < (playRect.x + playRect.w) && mousePos.x>playRect.x && mousePos.y < (playRect.y + playRect.h) && mousePos.y > playRect.y)
+		SDL_SetTextureColorMod(pText, 255, 212, 0);
 	else SDL_SetTextureColorMod(pText,0, 0,0);
 
 	//Sound
-	if (mousePos.x < (soundRect.x + soundRect.w) && mousePos.x>soundRect.x && mousePos.y < (soundRect.y + soundRect.y) && mousePos.y > soundRect.y) {
-		SDL_SetTextureColorMod(sText, 0, 255, 0);
-
-		if (mouse.button = SDL_BUTTON_LEFT) isRunning = false;
-	}	else SDL_SetTextureColorMod(sText, 32, 155, 13);
-
+	switch (Mix_PausedMusic()) {
+	case true:
+		if (mousePos.x < (soundRect.x + soundRect.w) && mousePos.x>soundRect.x && mousePos.y < (soundRect.y + soundRect.h) && mousePos.y > soundRect.y) {
+			SDL_SetTextureColorMod(sText, 0, 250, 0);
+			if (cLeft) Mix_PlayMusic(sTrack,-1);
+		}
+		else SDL_SetTextureColorMod(sText, 250,0,0);
+		break;
+	case false:
+		if (mousePos.x < (soundRect.x + soundRect.w) && mousePos.x>soundRect.x && mousePos.y < (soundRect.y + soundRect.h) && mousePos.y > soundRect.y) {
+			SDL_SetTextureColorMod(sText, 250, 0,0);
+			if (cLeft) Mix_PauseMusic();
+		}
+		else SDL_SetTextureColorMod(sText, 0, 250, 0);
+		break;
+	}
+	
+	cLeft = false;
 }
